43c63935eba0daf1011f9e14b39621bd170620ab
Luke Wagner <lukedwagner01@gmail.com>
2024-01-12 12:39:52 -0800

    Revert Program.cs to earlier version

--- src/Program.cs
+++ src/Program.cs
@@ -2,22 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        C4AITrainer AI_1 = new C4AITrainer(true);
-        C4AITrainer AI_2 = new C4AITrainer(false);
-
-        AI_2.Play();
-
-        //C4AITrainer? winner = AIDueler.PlayMatch(ref AI_1, ref AI_2, true);
-        //if (winner != null){
-        //    Console.WriteLine(winner.ToString());
-        //}
-
-        /*
         Connect4 baseGame = new Connect4();
         C4SmartAI gameWithSmartAI = new C4SmartAI();
 
         //baseGame.Play();
         gameWithSmartAI.Play();
-        */
     }
 }
